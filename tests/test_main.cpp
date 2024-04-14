295717d6fa317d5e01866c075d57b0a42b37731a
FriedHamCheese <160022244+FriedHamCheese@users.noreply.github.com>
2024-04-14 15:59:08 +0700

    An exception catch in test_main

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,10 +3,18 @@
 #include <iostream>
 
 int main(){
-	std::clog << "Initiating test. If an [ALL CLEAR] is not displayed, the test has failed.\n";
-	
-	test_suite_task_io();
-	
-	std::clog << "[ALL CLEAR]: all tests verified.\n";
-	return 0;
+	try{	
+		std::clog << "Initiating test. If an [ALL CLEAR] is not displayed, the test has failed.\n";
+		test_suite_task_io();
+		std::clog << "[ALL CLEAR]: all tests verified.\n";
+		return 0;
+	}
+	catch(const std::exception& excp){
+		std::cerr << "[Test Failed]: caught an unspecified exception.\n";
+		std::cerr << "\tMessage of the exception: " << excp.what() << '\n';
+	}
+	catch(...){
+		std::cerr << "[Test Failed]: caught an unspecified throw.\n";
+	}	
+	return -1;
 }
